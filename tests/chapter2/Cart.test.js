843ee7c0fb73bd153c3f82c6f9758f8ca3fcd9fc
Edward Lopez-Ramos <lopez.edward@outlook.com>
2022-03-20 21:18:49 -0400

    Jest testing used

--- tests/chapter2/Cart.test.js
+++ tests/chapter2/Cart.test.js
@@ -1,13 +1,16 @@
-const assert = require("assert");
 const Cart = require("../../src/chapter2/Cart");
 
-const cart = new Cart();
-cart.addToCart("cheesecake");
 // cart.addToCart("pepper");
 
-assert.deepStrictEqual(cart.items, ["cheesecake"]);
-console.log("The addToCart function can add an item to the cart");
+test("The addToCart function can add an item to the cart", () => {
+  const cart = new Cart();
+  cart.addToCart("cheesecake");
+  expect(cart.items).toEqual(["cheesecake"]);
+});
 
-cart.removeFromCart("cheesecake");
-assert.deepStrictEqual(cart.items, []);
-console.log("The removeFromCart function can remove an item from the cart");
+test("The removeFromCart function can remove an item from the cart", () => {
+  const cart = new Cart();
+  cart.addToCart("cheesecake");
+  cart.removeFromCart("cheesecake");
+  expect(cart.items).toEqual([]);
+});
