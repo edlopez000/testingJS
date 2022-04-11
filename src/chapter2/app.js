04d5f1abbfb00d7498f8576d0d3cb05e67c2995e
Edward Lopez-Ramos <lopez.edward@outlook.com>
2022-04-11 02:16:08 -0400

    Refactored app.js to fit Express style

---
+++ src/chapter2/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+carts = new Map();
+
+app.get("/carts/:username/:items", (req, res) => {
+  console.log(`GET Params: ${req.params}`);
+  const { username, items } = req.params;
+  const cart = carts.get(username);
+  cart
+    ? res.send(cart)
+    : res.status(404).send("Didn't find anything with that username!");
+});
+
+app.post("/carts/:username/items/:item", (req, res) => {
+  console.log(`POST Params: ${req.params}`);
+  const { username, item } = req.params;
+  const newItems = (carts.get(username) || []).concat(item);
+  carts.set(username, newItems);
+  res.send(newItems);
+});
+
+module.exports = { app, port };
--- src/chapter2/server.js
+++ src/chapter2/server.js
@@ -1,26 +1,5 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-
-carts = new Map();
-
-app.get("/carts/:username/:items", (req, res) => {
-  console.log(`GET Params: ${req.params}`);
-  const { username, items } = req.params;
-  const cart = carts.get(username);
-  cart
-    ? res.send(cart)
-    : res.status(404).send("Didn't find anything with that username!");
-});
-
-app.post("/carts/:username/items/:item", (req, res) => {
-  console.log(`POST Params: ${req.params}`);
-  const { username, item } = req.params;
-  const newItems = (carts.get(username) || []).concat(item);
-  carts.set(username, newItems);
-  res.send(newItems);
-});
+const { app, port } = require("./app");
 
 app.listen(port, () => {
-  console.log(`App is listening on port ${port}`);
+  console.log(`App listening on port ${port} `);
 });
